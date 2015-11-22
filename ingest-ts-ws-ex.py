248b2ce47ddc8227b48176e18957745ba1dc06a7
Ricardo <ricardo.breder@ge.com>
2015-11-22 04:47:31 -0800

    added yaml configuration and auto token fetch option

--- ingest-ts-ws-ex.py
+++ ingest-ts-ws-ex.py
@@ -8,6 +8,9 @@ import csv
 import os
 import argparse
 import textwrap
+import requests
+import json
+import yaml
 from collections import defaultdict
 
 # Parser for input arguments
@@ -40,26 +43,38 @@ parser = argparse.ArgumentParser(
         '''))
 
 parser.add_argument("data", help="csv file with time-series data")
-parser.add_argument("tss", help="time-series wss url")
-parser.add_argument("zone", help="time-series service instance id")
-parser.add_argument("token", help="predix cloud UAA token")
+parser.add_argument("--tss", dest='tss', default='', help="time-series wss url")
+parser.add_argument("--zone", dest='zone', default='', help="time-series service instance id")
+parser.add_argument("--uaa", dest='uaa', default='', help="predix UAA issuerId (Token URI)")
+parser.add_argument("--client", dest='client', default='', help="predix UAA Client")
+parser.add_argument("--secret", dest='secret', default='', help="predix UAA Client secret")
+parser.add_argument("--username", dest='username', default='', help="username from Predix UAA with access to time-series")
+parser.add_argument("--password", dest='password', default='', help="password from Predix UAA with access to time-series")
+parser.add_argument("--token", dest='token', default='', help="specify the predix UAA token with access to time-series")
 parser.add_argument("-d", "--delimiter", dest='delimiter', default=';', help="specify the delimiter character. Default is ;")
 parser.add_argument("-t", "--timestamp", dest='timestamp', default="%m/%d/%Y %I:%M:%S %p", help="specify the timestamp format following strftime(3) documentation")
 parser.add_argument("-s", "--datapoints", dest='dpsize', default='500', help="specify the number of points per message")
-parser.add_argument("-ei", dest='eni', default='1', help="specify the index of the equipment name column in the csv")
-parser.add_argument("-ti", dest='tni', default='2', help="specify the index of the tag name column in the csv")
-parser.add_argument("-di", dest='tsi', default='3', help="specify the index of the timestamp column in the csv")
-parser.add_argument("-vi", dest='vi', default='4', help="specify the index of the value column in the csv")
+parser.add_argument("--ei", dest='eni', default='1', help="specify the index of the equipment name column in the csv")
+parser.add_argument("--ti", dest='tni', default='2', help="specify the index of the tag name column in the csv")
+parser.add_argument("--di", dest='tsi', default='3', help="specify the index of the timestamp column in the csv")
+parser.add_argument("--vi", dest='vi', default='4', help="specify the index of the value column in the csv")
 parser.add_argument("--solH", dest='solH', default='', help="specify the SOL high limit")
 parser.add_argument("--solL", dest='solL', default='', help="specify the SOL low limit")
 parser.add_argument("--dBand", dest='dBand', default='', help="specify the deadband")
 parser.add_argument("--dSecs", dest='dSecs', default='', help="specify the deadband wait time in miliseconds")
+parser.add_argument("--es", dest='excursionSvc', default='', help="specify the service URI to send the excursions")
+parser.add_argument("-y", dest='yaml', default='', help="specify a yaml file with the configuration")
 args = parser.parse_args()
 
 data = args.data
-tss = args.tss
-zone = args.zone
-token = args.token
+tsUri = args.tss
+tsZone = args.zone
+uaaUri = args.uaa
+uaaUsername = args.username
+uaaSecret = args.secret
+uaaClient = args.client
+uaaPassword = args.password
+uaaToken = args.token
 delimiter = args.delimiter
 timestamp = args.timestamp
 dpsize = int(args.dpsize)
@@ -71,17 +86,19 @@ eni = int(args.eni)
 tni = int(args.tni)
 tsi = int(args.tsi)
 vi = int(args.vi)
+excursionSvc = args.excursionSvc
+yamlFile = args.yaml
 
 def on_message(ws, message):
-    if message["statuscode"] != 202:
+    if json.JSONDecoder().decode(message)["statusCode"] != 202:
         print("Error sending packet to time-series service")
-        print("Message: " + message)
+        print(message)
         sys.exit(1)
     else:
-        print("Message: " + message)
+        print("Packet Sent")
 
 def on_error(ws, error):
-    print("Error: " + error)
+    print(error)
 
 def on_close(ws):
     print("--- Socket Closed ---")
@@ -99,23 +116,23 @@ def on_open(ws):
         meter = ""
         payloads = []
         nCount = 0
-        excursion = {"start": 0, "end": 0, "total": 0, "tag": "", "active": False, "SOL": "", "SDL": "", "count": 0}
+        excursion = {"start": 0, "end": 0, "duration": 0, "tag": "", "active": False, "type": "", "count": 0, "source": "OSIPI"}
 
         print("Generating packets with " + str(dpsize) + " data points...")
 
-        for i, row in df.iterrows():
+        for index, row in df.iterrows():
             # Create the packets to send it over WS
             # Define the tag name if none exists
             if meter == "":
-                tagname = row[2]
-                equipname = row[1]
+                tagname = row[tni]
+                equipname = row[eni]
                 meter = equipname + "_" + tagname
 
             # If current tag name is different than the tag name from the file, define another tag name
             elif meter != equipname + "_" + tagname:
                 payloads.append(payload(ws, meter, datapoints, m))
-                tagname = row[2]
-                equipname = row[1]
+                tagname = row[tni]
+                equipname = row[eni]
                 meter = equipname + "_" + tagname
                 m += 1
                 i = 0
@@ -130,96 +147,97 @@ def on_open(ws):
 
             # Verifies if the value is a valid number or don't add the point
             try:
-                value = float(row[4])
-                tstamp = calendar.timegm(time.strptime(row[3], timestamp)) * 1000
-                datapoints.append([tstamp, value])
+                value = float(row[vi])
             except:
                 value = 0.0
                 i += 1
                 continue
 
+            tstamp = calendar.timegm(time.strptime(row[tsi], timestamp)) * 1000
+            datapoints.append([tstamp, value])
+
             # Verifies for SOL excursions on High limit
             if value > solH:
 
                 # Verifies for active SOL High excursion
-                if excursion['SOL'] == "High":
-                    excursion['total'] += (tstamp - excursion['end'])
+                if excursion['type'] == "SOL High":
+                    excursion['duration'] += (tstamp - excursion['end'])
                     excursion['end'] = tstamp
                     excursion['count'] += 1
                 else:
                     # Deactivates SOL Low excursion and send it
-                    if excursion['SOL'] == "Low":
+                    if excursion['type'] == "SOL Low":
                         excursion['active'] = False
                         # if nCount == 0:
-                        excursion['total'] += (tstamp - excursion['end'])
+                        excursion['duration'] += (tstamp - excursion['end'])
                         excursion['end'] = tstamp
                         excursion['count'] += 1
 
-                        # Only add if total time is greater than 60 seconds
-                        if excursion['total'] > 60000:
+                        # Only add if duration time is greater than 60 seconds
+                        if excursion['duration'] > 60000:
                             sendExcursions(excursion)
 
                     # Create new SOL High excursion
                     excursion['end'] = tstamp
                     excursion['start'] = tstamp
-                    excursion['total'] = 0
+                    excursion['duration'] = 0
                     excursion['active'] = True
                     excursion['tag'] = meter
-                    excursion['SOL'] = "High"
+                    excursion['type'] = "SOL High"
                     excursion['count'] = 1
 
             # Verifies for SOL excursions on Low Limit
             elif value < solL:
 
                 # Verifies for active SOL Low excursion
-                if excursion['SOL'] == "Low":
-                    excursion['total'] += (tstamp - excursion['end'])
+                if excursion['type'] == "SOL Low":
+                    excursion['duration'] += (tstamp - excursion['end'])
                     excursion['end'] = tstamp
                     excursion['count'] += 1
                 else:
                     # Deactivates SOL High excursion and send it
-                    if excursion['SOL'] == "High":
+                    if excursion['type'] == "SOL High":
                         excursion['active'] = False
-                        # if nCount == 0:
-                        excursion['total'] += (tstamp - excursion['end'])
+                        
+                        excursion['duration'] += (tstamp - excursion['end'])
                         excursion['end'] = tstamp
                         excursion['count'] += 1
 
-                        # Only add if total time is greater than 60 seconds
-                        if excursion['total'] > 60000:
+                        # Only add if duration time is greater than 60 seconds
+                        if excursion['duration'] > 60000:
                             sendExcursions(excursion)
 
                     # Create new SOL Low excursion
                     excursion['end'] = tstamp
                     excursion['start'] = tstamp
-                    excursion['total'] = 0
+                    excursion['duration'] = 0
                     excursion['active'] = True
                     excursion['tag'] = meter
-                    excursion['SOL'] = "Low"
+                    excursion['type'] = "SOL Low"
                     excursion['count'] = 1
 
             # If not excursion
             else:
                 # If there's an active excursion, de-activates it and send it
-                if excursion['SOL'] != "" and (value > (solL + dBand) and (tstamp - excursion['end']) > dSecs):
+                if excursion['type'] != "" and (value > (solL + dBand) and (tstamp - excursion['end']) > dSecs):
                     excursion['active'] = False
-                    # excursion['total'] += (tstamp - excursion['end'])
+                    # excursion['duration'] += (tstamp - excursion['end'])
                     # excursion['end'] = tstamp
 
-                    # Only add if total time is greater than 60 seconds
-                    if excursion['total'] > 60000:
+                    # Only add if duration time is greater than 60 seconds
+                    if excursion['duration'] > 60000:
                         sendExcursions(excursion)
                     
                     # Reset excursion object
                     excursion['end'] = 0
                     excursion['start'] = 0
-                    excursion['total'] = 0
+                    excursion['duration'] = 0
                     excursion['tag'] = ""
-                    excursion['SOL'] = ""
+                    excursion['type'] = ""
                     excursion['count'] = 0
 
-                elif excursion['SOL'] != "" and (value < (solL + dBand) and (tstamp - excursion['end']) < dSecs):
-                    excursion['total'] += (tstamp - excursion['end'])
+                elif excursion['type'] != "" and (value < (solL + dBand) and (tstamp - excursion['end']) < dSecs):
+                    excursion['duration'] += (tstamp - excursion['end'])
                     excursion['end'] = tstamp
                     excursion['count'] += 1
                     # nCount = 1
@@ -231,10 +249,10 @@ def on_open(ws):
             payloads.append(payload(ws, meter, datapoints, m+1))
 
         # Send payloads
-        # sendPayload(ws, payloads)
+        sendPayload(ws, payloads)
 
         # Send last excursion
-        if excursion['SOL'] != "" and excursion['total'] > 60000:
+        if excursion['type'] != "" and excursion['duration'] > 60000:
             sendExcursions(excursion)
 
     else:
@@ -282,25 +300,85 @@ def sendPayload(ws, payloads):
     thread.start_new_thread(run, ())
 
 def sendExcursions(excursions):
-    print excursions
-    pass
+    print("Sending Excursion...")
+    exs = []
+    exs.append(excursions)
+    request = requests.post(excursionSvc, data = json.JSONEncoder().encode(exs))
+    if request.status_code == requests.codes.ok:
+        print("Excursion sent")
+    else:
+        print("Error sending excursion")
+        request.raise_for_status()
 
+def getToken():
+    uri = uaaUri
+    payload = {"grant_type": "password", "username": uaaUsername, "password": uaaPassword}
+    auth = requests.auth.HTTPBasicAuth(uaaClient, uaaSecret)
+    request = requests.post(uri, data=payload, auth=auth)
+    if request.status_code == requests.codes.ok:
+        return json.JSONDecoder().decode(request.text)["access_token"]
+    else:
+        print("Error requesting token")
+        request.raise_for_status()
 
 if __name__ == "__main__":
+    if yamlFile != '':
+        if os.path.isfile(yamlFile):
+            with open(yamlFile, 'r') as stream:
+                config = yaml.load(stream)
+                tsUri = config["time-series"]["uri"]
+                tsZone = config["time-series"]["zone"]
+                uaaToken = config["uaa"]["token"]
+                uaaUri = config["uaa"]["uri"]
+                uaaClient = config["uaa"]["client"]
+                uaaSecret = config["uaa"]["secret"]
+                uaaUsername = config["uaa"]["username"]
+                uaaPassword = config["uaa"]["password"]
+                if "csv" in config.keys():
+                    csvConfig = config["csv"]
+                    if "delimiter" in csvConfig.keys():
+                        delimiter = csvConfig["delimiter"]
+                    if "timestamp" in csvConfig.keys():
+                        timestamp = csvConfig["timestamp"]
+                    if "packetsize" in csvConfig.keys():
+                        dpsize = int(csvConfig["packetsize"])
+                    if "indexes" in csvConfig.keys():
+                        eni = int(csvConfig["indexes"]["equipment"])
+                        tni = int(csvConfig["indexes"]["tag"])
+                        tsi = int(csvConfig["indexes"]["timestamp"])
+                        vi = int(csvConfig["indexes"]["value"])
+                if "excursion" in config.keys():
+                    exConfig = config["excursion"]
+                    if "uri" in exConfig.keys():
+                        excursionSvc = exConfig["uri"]
+                print("Configuration file " + yamlFile + " loaded")
+        else:
+            print("The file " + yamlFile + " doesn't exist or you don't have permission to access it")
+            print("Terminating...")
+            sys.exit(1)
+    elif tsUri == '' or tsZone == '' or uaaUri == '' or uaaClient == '' or uaaSecret == '' or uaaUsername == '' or uaaPassword == '':
+        print("Please either use a yaml file with option -y")
+        print("or specify the parements -tss -zone -uaa -client -secre -username -password")
+        print("Terminating...")
+        sys.exit(1)
+
     websocket.enableTrace(True)
-    host = tss
+    host = tsUri
+
+    if uaaToken == '':
+        uaaToken = getToken()
+
     headers = {
-                'Authorization:bearer ' + token,
-                'Predix-Zone-Id:' + zone,
+                'Authorization:bearer ' + uaaToken,
+                'Predix-Zone-Id:' + tsZone,
                 'Origin:http://localhost/'
     }
-    on_open("")
-    # ws = websocket.WebSocketApp(
-    #                             host,
-    #                             header = headers,
-    #                             on_message = on_message,
-    #                             on_error = on_error,
-    #                             on_close = on_close
-    # )
+    ws = websocket.WebSocketApp(
+                                host,
+                                header = headers,
+                                on_message = on_message,
+                                on_error = on_error,
+                                on_close = on_close
+    )
     ws.on_open = on_open
     ws.run_forever()
